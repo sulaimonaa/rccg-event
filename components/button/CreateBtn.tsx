6dd97dbbb414a925c5c01942bc56230459c4fc59
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2026-06-10 13:35:21 +0100

    added interaction to the create page

--- components/button/CreateBtn.tsx
+++ components/button/CreateBtn.tsx
@@ -1,10 +1,17 @@
+'use client'
+
+import {useState} from "react";
+
 const CreateBtn = () => {
+    const [loading, isLoading] = useState(false);
+
     return (
         <button
             type="submit"
-            className="w-full bg-black/50 text-white py-3 rounded-lg hover:bg-black/60"
+            className="w-full bg-black/50 text-white py-3 rounded-lg hover:bg-black/60 hover:cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed"
+            disabled={loading}
         >
-            Create Business
+            {loading ? "Creating business" : "Create Business"}
         </button>
     )
 }
