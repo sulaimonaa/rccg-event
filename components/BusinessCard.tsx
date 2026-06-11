94e9400a2e27b6df656c7a47ac784a3200b14be2
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2026-06-11 23:18:24 +0100

    fix redirect and form reset issues

--- components/BusinessCard.tsx
+++ components/BusinessCard.tsx
@@ -14,7 +14,9 @@ interface Props {
 const BusinessCard = ({business, image, service, contact, email}: Props) => {
     return (
         <Link href={`/business/${business}`} className={"flex flex-col items-center bg-cyan-950/20 rounded-b-lg"}>
-            <Image src={image} alt={`${business} logo`} className={"mb-4 rounded-lg object-cover bg-white"} width={400} height={200} />
+            <div className={"w-full bg-white flex items-center justify-center h-50 overflow-hidden rounded-t-lg"}>
+                <Image src={image} alt={`${business} logo`} className={"mb-4 rounded-lg object-cover bg-white w-full"} width={400} height={200} />
+            </div>
             <div className={'p-4 '}>
                 <h2 className={"text-xl font-bold mb-2 text-white flex items-center justify-start gap-2"}><FaUserCircle />{business}</h2>
                 <p className={"text-gray-400 mb-1 flex items-center justify-start gap-2"}><FaBuilding /> {service}</p>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     return (
         <div className="w-full py-4 m-0 bg-black/10">
             <div className={"lg:w-10/12 container mx-auto flex items-center justify-between px-4"}>
-                <Link href={""}>
-                    <Image src={"/Rccglogo.png"} alt={"RCCG logo"} width={22} height={22} />
+                <Link href={"/"}>
+                    <div className={"text-white text-lg font-bold flex items-center justify-center gap-2"}>
+                        <Image src={"/Rccglogo.png"} alt={"RCCG logo"} width={22} height={22} /> WAP
+                    </div>
                 </Link>
-                <div className={"text-white text-lg font-bold flex items-center justify-center gap-2"}>
-                   <Image src={"/Rccglogo.png"} alt={"RCCG logo"} width={22} height={22}  /> WAP</div>
                 <nav className={"space-x-4"}>
                     <Link href="/" className={"text-white hover:text-gray-300"}>Home</Link>
                     <Link href="/businesses" className={"text-white hover:text-gray-300"}>Businesses</Link>
